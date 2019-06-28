95ba7e838654d7b507d0131b24784a6c76c80b70
Erik Zscheile <erik.zscheile@gmail.com>
2019-06-28 21:34:27 +0200

    v0.1.0
    +ChunkedFile
    +ContinuableFile
    +read_part_from_file
    
    API BREAK (get_file_len doesn't sanitize isize anymore)
    +buffer seek

---
+++ src/backend.rs
@@ -0,0 +1,101 @@
+use boolinator::Boolinator;
+use std::{fs::File, io, io::Read, io::Seek};
+use crate::{FileHandle, FileHandle::*, get_file_len, LengthSpec};
+
+// private interface
+
+fn open_as_mmap(fh: &File, offset: u64, len: usize) -> io::Result<memmap::Mmap> {
+    Ok(unsafe {
+        // NOTE: replace map_copy?.make_read_only? with map_copy_read_only?
+        // once issue danburkert/memmap-rs#81 is fixed
+        memmap::MmapOptions::new()
+            .offset(offset)
+            .len(len)
+            .map_copy(&fh)?
+            .make_read_only()?
+    })
+}
+
+#[must_use]
+enum EvaluatedLength {
+    ELUnknown,
+    ELImpossible,
+    ELBounded(usize),
+}
+
+use self::EvaluatedLength::*;
+
+/// @param flen_hint : used to cache the call to [`get_file_len`]
+fn eval_length(fh: &File, offset: u64, lenspec: LengthSpec, flen_hint: Option<u64>) -> EvaluatedLength {
+    let maxlen_i = std::isize::MAX as usize;
+    let is_untileof = lenspec.bound.is_none();
+    let x = lenspec.bound.unwrap_or(maxlen_i);
+    let maxlen_f = flen_hint.or_else(|| get_file_len(&fh))
+        .map(|lx| (lx - offset) as usize)
+        .unwrap_or(maxlen_i);
+    let maxlen = std::cmp::min(maxlen_f, maxlen_i);
+    return
+        if x > maxlen {
+            // ensure maximum length
+            if lenspec.is_exact { ELImpossible } else { ELBounded(maxlen) }
+        } else {
+            if is_untileof { ELUnknown } else { ELBounded(x) }
+        };
+}
+
+/// Reads a part of the file contents,
+/// use this if the file is too big and needs to be read in parts,
+/// starting at [`offset`] and until the given LengthSpec is met.
+pub(crate) fn read_part_from_file_intern(fh: &mut File, offset: u64, len: LengthSpec, flen_hint: Option<u64>) -> io::Result<FileHandle> {
+    let evl = eval_length(&fh, offset, len, flen_hint);
+    match evl {
+        ELImpossible => {
+            return Err(io::Error::new(io::ErrorKind::InvalidInput, "length is too big"));
+        },
+        ELBounded(0) => {
+            return Ok(Buffered(Vec::new()));
+        },
+        ELBounded(lx) => {
+            // do NOT try to map the file if the size is unknown
+            if let Some(ret) = open_as_mmap(&fh, offset, lx).ok() {
+                return Ok(Mapped(ret));
+            }
+        },
+        ELUnknown => {},
+    }
+
+    // use Buffered
+    use std::hint::unreachable_unchecked;
+    fh.seek(io::SeekFrom::Start(offset))?;
+    let mut bfr = io::BufReader::new(fh);
+    let mut contents = Vec::new();
+    match evl {
+        ELImpossible => unsafe { unreachable_unchecked() },
+        ELBounded(lx) => {
+            contents.resize(lx, 0);
+            if len.is_exact {
+                bfr.read_exact(&mut contents)?;
+            } else {
+                bfr.read(&mut contents)?;
+            }
+        },
+        ELUnknown => {
+            if let Err(x) = bfr.read_to_end(&mut contents) {
+                if len.is_exact || contents.is_empty() {
+                    return Err(x);
+                }
+            }
+        },
+    };
+    contents.shrink_to_fit();
+    Ok(Buffered(contents))
+}
+
+pub(crate) fn do_offset_add(offset: u64, x: i64) -> Option<u64> {
+    if x < 0 {
+        let xn = (-x) as u64;
+        (xn <= offset).as_some_from(|| offset - xn)
+    } else {
+        Some(offset + (x as u64))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,14 @@
+#![cfg_attr(feature = "seek_convenience", feature(seek_convenience))]
+
 extern crate boolinator;
 extern crate memmap;
+mod backend;
 
-use boolinator::Boolinator;
-use memmap::{Mmap, MmapOptions};
-use std::{fs, io, io::Read, isize};
-
-// private interface
-
-fn open_as_mmap(fh: &fs::File, len: usize) -> io::Result<Mmap> {
-    Ok(unsafe {
-        // NOTE: replace map_copy?.make_read_only? with map_copy_read_only?
-        // once issue danburkert/memmap-rs#81 is fixed
-        MmapOptions::new()
-            .len(len)
-            .map_copy(&fh)?
-            .make_read_only()?
-    })
-}
+use memmap::Mmap;
+use std::{fs, io};
+use backend::*;
 
 // public interface
-
-/// Returns the length of the file,
-/// and is baed upon [`memmap::MmapOptions::get_len()`].
-/// It sanitises the fact that mapping a slice greater than isize::MAX
-/// has undefined behavoir.
-pub fn get_file_len(fh: &fs::File) -> Option<usize> {
-    fh.metadata()
-        .ok()
-        .map(|x| x.len())
-        .and_then(|len| (len != 0 && len <= (isize::MAX as u64)).as_some(len as usize))
-}
 
 /// buffered or mmapped file contents handle
 pub enum FileHandle {
@@ -51,17 +30,177 @@ impl FileHandle {
     }
 }
 
+impl std::ops::Deref for FileHandle {
+    type Target = [u8];
+
+    #[inline]
+    fn deref(&self) -> &[u8] {
+        self.get_slice()
+    }
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
+pub struct LengthSpec {
+    bound: Option<usize>,
+    is_exact: bool,
+}
+
+impl LengthSpec {
+    /// @param bound
+    ///   ? (read at most $n bytes)
+    ///   : (read until EOF)
+    /// @param is_exact
+    ///   ? (request exactly length or fail)
+    ///   : (request biggest readable slice with length as upper bound)
+    pub fn new(bound: Option<usize>, is_exact: bool) -> Self {
+        Self { bound, is_exact }
+    }
+}
+
+impl std::default::Default for LengthSpec {
+    /// read as much as possible
+    #[inline]
+    fn default() -> Self {
+        Self { bound: None, is_exact: false }
+    }
+}
+
+/// Returns the length of the file,
+/// and is based upon [`memmap::MmapOptions::get_len()`].
+/// It doesn't sanitize the fact that mapping a slice greater than isize::MAX
+/// has undefined behavoir.
+pub fn get_file_len(fh: &fs::File) -> Option<u64> {
+    fh.metadata()
+        .ok()
+        .map(|x| x.len())
+}
+
 /// Reads the file contents
 pub fn read_from_file(fh: io::Result<fs::File>) -> io::Result<FileHandle> {
-    let fh = fh?;
-    let len = get_file_len(&fh);
+    let mut fh = fh?;
+    let lns = LengthSpec { bound: None, is_exact: true };
+    read_part_from_file(&mut fh, 0, lns)
+}
 
-    // do NOT try to map the file if the size is unknown
-    if let Some(ret) = len.and_then(|len| open_as_mmap(&fh, len).ok()) {
-        return Ok(Mapped(ret));
+/// Reads a part of the file contents,
+/// use this if the file is too big and needs to be read in parts,
+/// starting at offset and until the given LengthSpec is met.
+/// if you want a more ergonomic interface, use [`ContinuableFile`] or [`ChunkedFile`].
+/// fh is a reference because this function is intended to be called multiple times
+#[inline]
+pub fn read_part_from_file(mut fh: &mut fs::File, offset: u64, len: LengthSpec) -> io::Result<FileHandle> {
+    read_part_from_file_intern(&mut fh, offset, len, None)
+}
+
+#[must_use]
+pub struct ContinuableFile {
+    file: fs::File,
+    flen: Option<u64>,
+    offset: u64,
+}
+
+#[must_use]
+pub struct ChunkedFile {
+    cf: ContinuableFile,
+    lns: LengthSpec,
+}
+
+impl ContinuableFile {
+    pub fn new(file: fs::File) -> Self {
+        let mut ret = Self { file, flen: None, offset: 0 };
+        ret.sync_len();
+        return ret;
     }
-    let mut contents = Vec::with_capacity(len.unwrap_or(0) + 1);
-    io::BufReader::new(fh).read_to_end(&mut contents)?;
-    contents.shrink_to_fit();
-    Ok(Buffered(contents))
+
+    pub fn to_chunks(self, lns: LengthSpec) -> ChunkedFile {
+        ChunkedFile { cf: self, lns }
+    }
+
+    pub fn sync_len(&mut self) {
+        self.flen = get_file_len(&self.file);
+    }
+
+    /// Tries to read the next part of the file contents
+    pub fn next(&mut self, lns: LengthSpec) -> io::Result<FileHandle> {
+        let rfh = read_part_from_file_intern(&mut self.file, self.offset, lns, self.flen)?;
+        self.offset += rfh.len() as u64;
+        Ok(rfh)
+    }
+}
+
+impl io::Seek for ContinuableFile {
+    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
+        let oore = Err(io::Error::new(io::ErrorKind::InvalidInput, "seek out of range"));
+        use io::SeekFrom::*;
+
+        match pos {
+            Start(x)   => self.offset = x,
+            End(x)     => {
+                let xn: u64 = (-x) as u64;
+                if (x > 0) || self.flen.is_none() || (xn > self.flen.unwrap()) {
+                    return oore;
+                }
+                self.offset = self.flen.unwrap() - xn;
+            },
+            Current(x) => {
+                match do_offset_add(self.offset, x) {
+                    Some(y) => self.offset = y,
+                    None    => return oore,
+                }
+            },
+        }
+
+        Ok(self.offset)
+    }
+
+    #[cfg(feature = "seek_convenience")]
+    fn stream_len(&mut self) -> io::Result<u64> {
+        match self.flen {
+            None    => Err(io::Error::new(io::ErrorKind::InvalidInput, "seek out of range")),
+            Some(x) => Ok(x),
+        }
+    }
+
+    #[cfg(feature = "seek_convenience")]
+    fn stream_position(&mut self) -> io::Result<u64> {
+        Ok(self.offset)
+    }
+}
+
+// getters
+impl ChunkedFile {
+    pub fn get_inner_ref(&mut self) -> &mut ContinuableFile {
+        &mut self.cf
+    }
+
+    pub fn into_inner(self) -> ContinuableFile {
+        self.cf
+    }
+
+    pub fn get_lns(&self) -> LengthSpec {
+        self.lns
+    }
+}
+
+impl std::iter::Iterator for ChunkedFile {
+    type Item = io::Result<FileHandle>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let item = self.cf.next(self.lns);
+        match item {
+            Ok(ref x) if x.len() == 0 => None,
+            _ => Some(item),
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let flen = self.cf.flen;
+        (flen.map(|x| (x - self.cf.offset as u64) as usize).unwrap_or(0), None)
+    }
+}
+
+impl io::Seek for ChunkedFile {
+    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
+        self.cf.seek(pos)
+    }
 }
